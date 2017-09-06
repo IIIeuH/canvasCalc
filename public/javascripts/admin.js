2a0bf3183b53db4c5e0083db192726e17af20cba
IIIeuH <shein.ev@gmail.com>
2017-09-06 17:44:45 +0300

    fix button

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,5 +1,6 @@
 $(function(){
     var select;
+    var string;
     $.ajax({
         url: "/itemType",
         type: 'GET',
@@ -11,48 +12,46 @@ $(function(){
             });
 
             select += '</select>';
+            string = '<tr class="str">' +
+                '<td>' +
+                '<div class="form-group">' +
+                '<input type="text" class="form-control itemName">' +
+                '</div>' +
+                '</td>' +
+                '<td>' +
+                '<div class="form-group">' +
+                select+
+                '</div>' +
+                '</td>' +
+                '<td>' +
+                '<div class="form-group">' +
+                '<input type="number" class="form-control itemHight">' +
+                '</div>' +
+                '</td>' +
+                '<td>' +
+                '<div class="form-group">' +
+                '<input type="number" class="form-control itemWidth">' +
+                '</div>' +
+                '</td>' +
+                '<td>' +
+                '<div class="form-group">' +
+                '<input type="number" class="form-control itemThin">' +
+                '</div>' +
+                '</td>' +
+                '<td>' +
+                '<div class="form-group">' +
+                '<input type="number" class="form-control itemPrice">' +
+                '</div>' +
+                '</td>' +
+                '</tr>';
         },
         error: function (err) {
             console.log(err);
         }
     });
 
-
-
     $('#addItem').click(function(){
 
-        var string = '<tr class="str">' +
-            '<td>' +
-            '<div class="form-group">' +
-            '<input type="text" class="form-control itemName">' +
-            '</div>' +
-            '</td>' +
-            '<td>' +
-            '<div class="form-group">' +
-             select+
-            '</div>' +
-            '</td>' +
-            '<td>' +
-            '<div class="form-group">' +
-            '<input type="number" class="form-control itemHight">' +
-            '</div>' +
-            '</td>' +
-            '<td>' +
-            '<div class="form-group">' +
-            '<input type="number" class="form-control itemWidth">' +
-            '</div>' +
-            '</td>' +
-            '<td>' +
-            '<div class="form-group">' +
-            '<input type="number" class="form-control itemThin">' +
-            '</div>' +
-            '</td>' +
-            '<td>' +
-            '<div class="form-group">' +
-            '<input type="number" class="form-control itemPrice">' +
-            '</div>' +
-            '</td>' +
-            '</tr>';
         $('.addItemtable').append(
             '<table class="table table-bordered addItemTable">' +
                 '<thead>' +
@@ -64,41 +63,9 @@ $(function(){
                     '<th>ItemPrice(Цена)</th>' +
                 '</thead>' +
                 '<tbody class="mainTable">' +
-                    '<tr class="str">' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                '<input type="text" class="form-control itemName">' +
-                            '</div>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                select+
-                            '</div>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                '<input type="number" class="form-control itemHight">' +
-                            '</div>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                '<input type="number" class="form-control itemWidth">' +
-                            '</div>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                '<input type="number" class="form-control itemThin">' +
-                            '</div>' +
-                        '</td>' +
-                        '<td>' +
-                            '<div class="form-group">' +
-                                '<input type="text" class="form-control itemPrice">' +
-                            '</div>' +
-                        '</td>' +
-                    '</tr>' +
+                    string+
                 '</tbody>'
         );
-
 
         $(this).addClass('hidden');
         $('#cancelItem').removeClass('hidden');
@@ -106,54 +73,56 @@ $(function(){
         $('#saveItem').removeClass('hidden');
         $('#cancelTable').removeClass('hidden');
 
-        //при клике на добавить строчку
-        $('#addTable').click(function(){
-            $('.mainTable').append(string);
-        });
 
-        //при клике на удалить строчку
-        $('#cancelTable').click(function(){
-            $('.str').last().remove();
-        });
-
-
-        //при клике на отмена
-        $('#cancelItem').click(function(){
-            $('.addItemTable').remove();
-            $('#addItem').removeClass('hidden');
-            $(this).addClass('hidden');
-            $('#addTable').addClass('hidden');
-            $('#saveItem').addClass('hidden');
-            $('#cancelTable').addClass('hidden');
-        });
-
-        //при клике сохранить
-        $('#saveItem').click(function(){
-            var data = [];
-            var col = [];
-            $('.str').each(function(i){
-                var mas = [];
-                col.push(i+1);
-                $(this).find('.form-group').children().each(function(){
-                    mas.push($(this).val());
-                });
-                data.push(mas);
-            });
-            console.log(data);
-            $.ajax({
-                url: '/admin/items/',
-                type: 'POST',
-                data: {data: JSON.stringify(data), col: JSON.stringify(col)},
-                success: function(data){
-                    window.location.reload();
-                },
-                error: function(err){
-                    console.log(err);
-                }
-            })
-        });
     });
 
+    //при клике на отмена
+    $('#cancelItem').click(function(){
+        $('.addItemTable').remove();
+        $('#addItem').removeClass('hidden');
+        $(this).addClass('hidden');
+        $('#addTable').addClass('hidden');
+        $('#saveItem').addClass('hidden');
+        $('#cancelTable').addClass('hidden');
+    });
+
+    //при клике сохранить
+    $('#saveItem').click(function(){
+        var data = [];
+        var col = [];
+        $('.str').each(function(i){
+            var mas = [];
+            col.push(i+1);
+            $(this).find('.form-group').children().each(function(){
+                mas.push($(this).val());
+            });
+            data.push(mas);
+        });
+        console.log(data);
+        $.ajax({
+            url: '/admin/items/',
+            type: 'POST',
+            data: {data: JSON.stringify(data), col: JSON.stringify(col)},
+            success: function(data){
+                window.location.reload();
+            },
+            error: function(err){
+                console.log(err);
+            }
+        })
+    });
+
+    //при клике на удалить строчку
+    $('#cancelTable').click(function(){
+        $('.str').last().remove();
+    });
+
+    //при клике на добавить строчку
+    $('#addTable').click(function(ev){
+        console.log($(this));
+        ev.preventDefault();
+        $('.mainTable').append(string);
+    });
     //клик на кнопку удаление в таблицу
     $('.close').click(function(){
         var name = $(this).data('name');
